b67392c68442a916b9ede460d1ca5e2440e921c4
Mathias Onea <mathias.onea@gmail.com>
2019-08-30 16:21:06 +0200

    removed unused test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,10 +2,8 @@
 
 namespace LaravelAt\ImageSanitize\Tests;
 
-use Illuminate\Http\Request;
-use LaravelAt\ImageSanitize\ImageSanitizeClass;
-use LaravelAt\ImageSanitize\Middlewares\ImageSanitizeMiddleware;
 use PHPUnit\Framework\TestCase;
+use LaravelAt\ImageSanitize\ImageSanitizeClass;
 
 class ExampleTest extends TestCase
 {
@@ -15,21 +13,5 @@ class ExampleTest extends TestCase
         $content = file_get_contents('../stubs/exploit.jpeg');
 
         $this->assertTrue((new ImageSanitizeClass)->detect($content));
-    }
-
-    /** @test */
-    public function true_is_true()
-    {
-        $request = new Request;
-
-        $request->merge([
-            'title' => 'Title is in mixed CASE',
-        ]);
-
-        $middleware = new ImageSanitizeMiddleware;
-
-        $middleware->handle($request, function ($req) {
-            //$this->assertEquals('Title is in Mixed Case', $req->title);
-        });
     }
 }
