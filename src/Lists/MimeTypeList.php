09bdd6a923057e211efc748dd7478d97743173b5
Mathias Onea <mathias.onea@gmail.com>
2019-08-31 09:50:56 +0200

    Refactoring to responsibilities

---
+++ src/Lists/MimeTypeList.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace LaravelAt\ImageSanitize\Lists;
+
+
+class MimeTypeList
+{
+    public function get(): array
+    {
+        return [
+            'image/jpeg',
+            'image/gif',
+            'image/png',
+            'image/bmp',
+        ];
+    }
+}
---
+++ src/Lists/PatternList.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace LaravelAt\ImageSanitize\Lists;
+
+
+class PatternList
+{
+    public function get(): array
+    {
+        return [
+            '<?php',
+            'phar',
+        ];
+    }
+}
