8ea08d892550e4d2efdd10f52ef186ec56a8b08a
Mathias Onea <mathias.onea@gmail.com>
2019-10-21 09:41:58 +0200

    Fixed StyleCI issues regarding https://github.styleci.io/analyses/XaRlEB

--- tests/ImageSanitizeFacadeTest.php
+++ tests/ImageSanitizeFacadeTest.php
@@ -6,11 +6,11 @@ use ImageSanitize;
 
 class ImageSanitizeFacadeTest extends TestCase
 {
-	/** @test */
-	public function it_provides_a_facade()
-	{
+    /** @test */
+    public function it_provides_a_facade()
+    {
         $content = file_get_contents(__DIR__.'/stubs/exploit.jpeg');
 
         $this->assertTrue(ImageSanitize::detect($content));
-	}
+    }
 }
